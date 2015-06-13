0a2134d4c947454dc1d8317f58a7585f14d994d5
tom--bo <otsuka.t.2013@gmail.com>
2015-06-13 21:45:06 +0900

    chromedriverに切り替え

--- kako_tenki.py
+++ kako_tenki.py
@@ -9,48 +9,51 @@ import unittest, time, re, time
 
 class Test(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
+
         self.driver.implicitly_wait(30)
         self.base_url = "http://www.data.jma.go.jp/"
         self.verificationErrors = []
-        self.accept_next_alert = True
     
     def test_(self):
+        t = 0.5
         driver = self.driver
         driver.get(self.base_url + "/gmd/risk/obsdl/index.php#")
-        time.sleep(1)
+        time.sleep(t)
         y = "1994"
         m = "-1"
         tmp = "0"
         driver.find_element_by_id("buttonDelAll").click()
-        time.sleep(1)
+        time.sleep(t)
         driver.find_element_by_xpath("(//*[@id=\"pr44\"])").click()
-        time.sleep(1)
+        time.sleep(t)
         driver.find_element_by_xpath("(//*[@id=\"stationMap\"]/div[9]/div)").click()
-        time.sleep(1)
+        time.sleep(t)
         driver.find_element_by_id("elementButton").click()
         driver.find_element_by_xpath("//*[@id='aggrgPeriod']/div/div[1]/div[1]/label/span").click()
-        time.sleep(1)
+        time.sleep(t)
         driver.find_element_by_id(u"気温").click()
-        time.sleep(1)
         driver.find_element_by_id(u"降水量").click()
-        time.sleep(1)
         driver.find_element_by_id(u"日照時間").click()
-        time.sleep(1)
         driver.find_element_by_id(u"風向・風速").click()
-        time.sleep(1)
         driver.find_element_by_id(u"全天日射量").click()
-        time.sleep(1)
         driver.find_element_by_id(u"現地気圧").click()
         driver.find_element_by_id(u"相対湿度").click()
         driver.find_element_by_id(u"蒸気圧").click()
+        time.sleep(t)
+        driver.find_element_by_id(u"現地気圧").click()
         driver.find_element_by_id("periodButton").click()
         Select(driver.find_element_by_name("iniy")).select_by_visible_text(y)
         Select(driver.find_element_by_name("endy")).select_by_visible_text(y)
-        Select(driver.find_element_by_name("inim")).select_by_visible_text(tmp)
-        Select(driver.find_element_by_name("endm")).select_by_visible_text("${finishm[{$m}]}")
-        Select(driver.find_element_by_name("endd")).select_by_visible_text("${finishd[{$m}]}")
+        time.sleep(t)
+        Select(driver.find_element_by_name("inim")).select_by_visible_text("1")
+        Select(driver.find_element_by_name("inid")).select_by_visible_text("1")
+        time.sleep(t)
+        Select(driver.find_element_by_name("endm")).select_by_visible_text("2")
+        Select(driver.find_element_by_name("endd")).select_by_visible_text("1")
+        time.sleep(t)
         driver.find_element_by_css_selector("#csvdl > img.rollover").click()
+        time.sleep(t*10)
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
