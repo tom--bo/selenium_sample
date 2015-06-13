2a10f087f5ce5b5e0104eab4347877e9c31e2940
tom--bo <otsuka.t.2013@gmail.com>
2015-06-13 22:01:43 +0900

    １地点で1995~2014の取得まで

--- kako_tenki.py
+++ kako_tenki.py
@@ -16,13 +16,15 @@ class Test(unittest.TestCase):
         self.verificationErrors = []
     
     def test_(self):
+        start_m_arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+        start_d_arr = [1,2,2,2,2,2,2,2,2,2,2,2]
+        end_m_arr =   [2,3,4,5,6,7,8,9,10,11,12,12]
+        end_d_arr =   [1,1,1,1,1,1,1,1,1,1,1,31]
         t = 0.5
         driver = self.driver
         driver.get(self.base_url + "/gmd/risk/obsdl/index.php#")
         time.sleep(t)
-        y = "1994"
-        m = "-1"
-        tmp = "0"
+
         driver.find_element_by_id("buttonDelAll").click()
         time.sleep(t)
         driver.find_element_by_xpath("(//*[@id=\"pr44\"])").click()
@@ -43,18 +45,20 @@ class Test(unittest.TestCase):
         time.sleep(t)
         driver.find_element_by_id(u"現地気圧").click()
         driver.find_element_by_id("periodButton").click()
-        Select(driver.find_element_by_name("iniy")).select_by_visible_text(y)
-        Select(driver.find_element_by_name("endy")).select_by_visible_text(y)
-        time.sleep(t)
-        Select(driver.find_element_by_name("inim")).select_by_visible_text("1")
-        Select(driver.find_element_by_name("inid")).select_by_visible_text("1")
-        time.sleep(t)
-        Select(driver.find_element_by_name("endm")).select_by_visible_text("2")
-        Select(driver.find_element_by_name("endd")).select_by_visible_text("1")
-        time.sleep(t)
-        driver.find_element_by_css_selector("#csvdl > img.rollover").click()
-        time.sleep(t*10)
-    
+        for y in range (1995, 2015):
+            Select(driver.find_element_by_name("iniy")).select_by_visible_text(str(y))
+            Select(driver.find_element_by_name("endy")).select_by_visible_text(str(y))
+            time.sleep(t)
+            for i in range(0,12):
+                Select(driver.find_element_by_name("inim")).select_by_visible_text(str(start_m_arr[i]))
+                Select(driver.find_element_by_name("inid")).select_by_visible_text(str(start_d_arr[i]))
+                Select(driver.find_element_by_name("endm")).select_by_visible_text(str(end_m_arr[i]))
+                Select(driver.find_element_by_name("endd")).select_by_visible_text(str(end_d_arr[i]))
+                time.sleep(t)
+                driver.find_element_by_css_selector("#csvdl > img.rollover").click()
+                time.sleep(t)
+            time.sleep(t*1)
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException, e: return False
